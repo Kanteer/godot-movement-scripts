364bd144efef260190e31be11b88f20a6448dc03
kanteer <59344076+Kanteer@users.noreply.github.com>
2022-03-17 09:49:40 -0400

    Update rpgMovementSystem.gd

--- rpgMovementSystem.gd
+++ rpgMovementSystem.gd
@@ -1,3 +1,8 @@
 var VELOCITY = 100
 var ACCELERATION = 100
 var FRICTION = 100
+
+func _process(delta):
+  Input.on_key_just_pressed("ui_down") - Input.on_key_just_pressed("ui_up")
+  Input.on_key_just_pressed("ui_left") - Input.on_key_just_pressed("ui_right")
+
