3d7f7d8e905d055a33f84f12ebcc29f3e912d196
kanteer <59344076+Kanteer@users.noreply.github.com>
2022-03-18 08:16:45 -0400

    Update rpgMovementSystem.gd

--- rpgMovementSystem.gd
+++ rpgMovementSystem.gd
@@ -33,4 +33,13 @@ func move_state(delta):
   var input_vector = Vector2.ZERO
   input_vector.x = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
   input_vector.y = Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right")
-
+  input_vector = inout_vector.normalized()
+  
+  if input_vector !=Vector2.ZERO:
+    #animationTree.set("parameters/Idle/blend_position", input_vector)
+    #animationTree.set("parameters/Run/blend_position", input_vector)
+    #animationState.travel("Run")
+    velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+  else:
+    #animationState.travel("Idle")
+    velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
