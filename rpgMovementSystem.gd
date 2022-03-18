599676a76825c6ddc4b0048c7ca54b9fa50c3344
kanteer <59344076+Kanteer@users.noreply.github.com>
2022-03-18 08:05:59 -0400

    Update rpgMovementSystem.gd

--- rpgMovementSystem.gd
+++ rpgMovementSystem.gd
@@ -1,8 +1,24 @@
 #extends Player
 
-var VELOCITY = 100
-var ACCELERATION = 100
-var FRICTION = 100
+const VELOCITY = 100
+const ACCELERATION = 100
+const FRICTION = 100
+
+enum {
+  MOVE,
+  ATTACK,
+  ROLL
+}
+
+var state = MOVE
+var velocity = Vector2.ZERO
+
+#onready var animationPlayer = $AnimationPlayer
+#onready var animationTree = $AnimationTree
+#onready var animationState = animationTree.get("parameters/playback")
+
+#func _ready():
+  animationTree.active = true
 
 func _process(delta):
   Input.on_key_just_pressed("ui_down") - Input.on_key_just_pressed("ui_up")
