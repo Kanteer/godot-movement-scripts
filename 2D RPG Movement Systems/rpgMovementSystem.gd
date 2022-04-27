62c089f2fe220040dcaa58ee422284761192b87f
kanteer <59344076+Kanteer@users.noreply.github.com>
2022-04-27 09:32:16 -0400

    Update rpgMovementSystem.gd

--- 2D RPG Movement Systems/rpgMovementSystem.gd
+++ 2D RPG Movement Systems/rpgMovementSystem.gd
@@ -1,5 +1,6 @@
 #extends Player
 
+#Variables. Edit them to change how the character moves.
 const VELOCITY = 100
 const ACCELERATION = 100
 const FRICTION = 100
@@ -20,6 +21,7 @@ var velocity = Vector2.ZERO
 #func _ready():
   #animationTree.active = true
 
+#State instances. These instances are what the character will do when a button is pressed, matching the states to make them more easier to manage and use later on in scripting.
 func _physics_process(delta):
   match state:
     MOVE:
@@ -28,20 +30,21 @@ func _physics_process(delta):
       attack_state (delta)
     ROLL:
       roll_state (delta)
-    
+
+#This makes the character move.
 func move_state(delta):
-  var input_vector = Vector2.ZERO
-  input_vector.x = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-  input_vector.y = Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right")
-  input_vector = inout_vector.normalized()
+  var input_vector = Vector2.ZERO #This grabs the vectors for the value and makes the value equal to 0. What this does is that it makes sure your character doesn't move when there isn't any inputs.
+  input_vector.x = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up") #Checks the inputs and moves the character on the x axis.
+  input_vector.y = Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right") #Checks the inputs and moves the character on the y axis.
+  input_vector = inout_vector.normalized() #This is self explanitory, it normalizes the values so you get only 0 or 1, nothing in between.
   
-  if input_vector !=Vector2.ZERO:
-    #animationTree.set("parameters/Idle/blend_position", input_vector)
-    #animationTree.set("parameters/Run/blend_position", input_vector)
-    #animationState.travel("Run")
-    velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+  if input_vector !=Vector2.ZERO: #If the input vector does not equal zero, then do this.
+    #animationTree.set("parameters/Idle/blend_position", input_vector) #Sets the animation to Idle.
+    #animationTree.set("parameters/Run/blend_position", input_vector) #Sets the animation to Run.
+    #animationState.travel("Run") #Sets the state to Run.
+    velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta) #Velocity is equal to the amount of velocity moving towards the direction.
   else:
     #animationState.travel("Idle")
-    velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+    velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta) #Sets all velocity and movement back to 0.
     
-move_and_slide(velocity * delta)
+move_and_slide(velocity * delta) #Collisions.
